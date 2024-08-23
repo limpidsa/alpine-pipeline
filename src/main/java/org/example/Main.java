24183be8cb4ea9edb9bd04fcb9729a442e7366bd
Anonymous <anonymous@anonymous>
2024-08-23 15:09:15 -0700

    a

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+
+import java.util.Map;
+import java.util.List;
+
+
+public class Main {
+    int ccc  ;
+    String abc;
+    int b=2;
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/CppcheckTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+public class CppcheckTest {
+
+    @Test
+    public void testFirstRun(){
+
+
+    }
+}
